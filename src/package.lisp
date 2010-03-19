3cbb94ba14d471f36b36a83b735cb2185dd7cda6
Gábor Melis <mega@retes.hu>
2010-03-19 09:57:14 +0100

    indenting

--- src/package.lisp
+++ src/package.lisp
@@ -5,30 +5,31 @@
 (cl:defpackage :micmac.metropolis-hastings
   (:nicknames #:micmac.mh)
   (:use :common-lisp)
-  (:export #:mc-chain
-           #:temperature
-           #:state
-           #:jump-to-sample
-           #:jump-to-sample*
-           #:prepare-jump-distribution
-           #:random-jump
-           #:log-probability-ratio
-           #:log-probability-ratio-to-jump-target
-           #:log-jump-probability-ratio
-           #:acceptance-probability
-           #:accept-jump
-           #:reject-jump
-           #:mayb-jump
-           #:jump
-           ;; MC3
-           #:mc3-chain
-           #:accept-swap-chain-states
-           #:reject-swap-chain-states
-           #:maybe-swap-chain-states
-           #:jump-between-chains
-           ;; Utility classes
-           #:enumerating-chain
-           #:tracing-chain)
+  (:export
+   #:mc-chain
+   #:temperature
+   #:state
+   #:jump-to-sample
+   #:jump-to-sample*
+   #:prepare-jump-distribution
+   #:random-jump
+   #:log-probability-ratio
+   #:log-probability-ratio-to-jump-target
+   #:log-jump-probability-ratio
+   #:acceptance-probability
+   #:accept-jump
+   #:reject-jump
+   #:mayb-jump
+   #:jump
+   ;; MC3
+   #:mc3-chain
+   #:accept-swap-chain-states
+   #:reject-swap-chain-states
+   #:maybe-swap-chain-states
+   #:jump-between-chains
+   ;; Utility classes
+   #:enumerating-chain
+   #:tracing-chain)
   (:documentation "Metroplis-Hastings MCMC."))
 
 (cl:defpackage :micmac.uct
